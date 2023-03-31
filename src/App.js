11fa141fab3674098ef91e405aee1fa79a7d2f96
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-03-31 14:45:19 +0300

    app.js tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import axios from "axios";
+import { response } from "msw";
+import React from "react";
+import { useState, useEffect } from "react";
+import Karakter from "./components/Karakter.js";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [karakterler, setKarakterler] = useState();
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/?page=")
+      .then((response) => {
+        setKarakterler(response.data.results);
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.log("Error" + error);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <Karakter karakterler={karakterler} />
     </div>
   );
-}
-
+};
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,18 @@
-// Karakter bileşeniniz buraya gelecek
+import React from "react";
+export default function Karakter(props) {
+  const { karakterler } = props;
+  return (
+    <div className="content">
+      <h1>KARAKTERLER</h1>
+      {karakterler.map((karakter) => (
+        <div className="karakterBilgileri">
+          <h3>{karakter.name}</h3>
+          <p>{karakter.height}</p>
+          <p>{karakter.mass}</p>
+          <p>{karakter.hair_color}</p>
+          <p>{karakter.skin_color}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
